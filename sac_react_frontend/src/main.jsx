807166cecaeba947e3e7cdf17b207b2bb75ace2f
Tarun S <tarun.scs406@gmail.com>
2026-07-29 10:23:59 +0530

    fix: resolve ReferenceError global is not defined by adding window.global definition in main.jsx

--- sac_react_frontend/src/main.jsx
+++ sac_react_frontend/src/main.jsx
@@ -1,3 +1,6 @@
+if (typeof window !== 'undefined') {
+  window.global = window;
+}
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
